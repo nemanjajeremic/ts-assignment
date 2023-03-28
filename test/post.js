c97a85561f0260962b90ee43df63f5357ca8a511
Nemanja Jeremic <jnemanja@hotmail.com>
2023-03-28 23:47:19 +0200

    improvements

--- test/post.js
+++ test/post.js
@@ -3,98 +3,100 @@ import { expect } from "chai";
 import { sendRequest } from "./utility.js";
 import testData from "../request-data.json" assert { type: "json" };
 
-describe("Team Sava", function () {
-  describe("Verify POST endpoint", function () {
-    after(async function () {
-      await sendRequest(testData.post.delete);
-    });
+describe("Verify POST endpoint", function () {
+  after(async function () {
+    await sendRequest(testData.post.delete);
+  });
 
-    it("Verify that post returns code 400 and status bad request if value is not in store", async function () {
-      let response;
-      try {
-        response = await axios.request(testData.post.negative);
-      } catch (error) {
-        expect(error.response.status, "Return code was not 400").to.equal(400);
-        expect(
-          error.response.statusText,
-          "Return code was not 'Bad Request'"
-        ).to.equal("Bad Request");
-      }
-    });
-
-    it("Verify that post returns added resource with proper content-type", async function () {
-      await sendRequest(testData.put.positive);
-
-      const response = await sendRequest(testData.post.positive);
-
-      expect(response.data).to.deep.equal(testData.post.positive.data);
+  it("Verify that post returns code 400 and status bad request if value is not in store", async function () {
+    let response;
+    try {
+      response = await axios.request(testData.post.negative);
+    } catch (error) {
+      expect(error.response.status, "Return code was not 400").to.equal(400);
       expect(
-        response.headers["content-type"],
-        "Content-type was not as expected"
-      ).to.equal("application/json");
-    });
+        error.response.statusText,
+        "Return code was not 'Bad Request'"
+      ).to.equal("Bad Request");
+    }
+  });
 
-    it("Verify that resource amount is increased by 1", async function () {
-      let getCurrentArraySize;
-      let getNewArraySize;
-      let response = await sendRequest(testData.get.positive);
-      getCurrentArraySize = await response.data.length;
+  it("Verify that post returns added resource with proper content-type", async function () {
+    await sendRequest(testData.put.positive);
 
-      if (getCurrentArraySize >= 10) {
-        throw Error(
-          "Maximum quota of 10 exceeded. Please delete some resources in order to add new resources."
-        );
-      }
-      response = await sendRequest(testData.get.positive);
-      getNewArraySize = await response.data.length;
-      console.log(getCurrentArraySize);
-      console.log(getNewArraySize);
+    const response = await sendRequest(testData.post.positive);
+
+    expect(response.data).to.deep.equal(testData.post.positive.data);
+    expect(
+      response.headers["content-type"],
+      "Content-type was not as expected"
+    ).to.equal("application/json");
+  });
+
+  it("Verify that resource amount is increased by 1 after using POST", async function () {
+    let getCurrentArraySize;
+    let getNewArraySize;
+    let response = await sendRequest(testData.get.positive);
+    getCurrentArraySize = await response.data.length;
+
+    if (getCurrentArraySize >= 10) {
+      throw Error(
+        "Maximum quota of 10 exceeded. Please delete some resources in order to add new resources."
+      );
+    }
+    response = await sendRequest(testData.get.positive);
+    getNewArraySize = await response.data.length;
+    expect(
+      getNewArraySize,
+      "Your back-end did not accept new resource, the amount of resources stayed the same"
+    ).to.equal(getCurrentArraySize + 1);
+  });
+
+  it("Verify that post errors if you provide non-json data", async function () {
+    let response;
+    try {
+      response = await axios.request(testData.post.negative);
+    } catch (error) {
+      expect(error.response.status, "Return code was not 400").to.equal(400);
       expect(
-        getNewArraySize,
-        "Your back-end did not accept new resource, the amount of resources stayed the same"
-      ).to.equal(getCurrentArraySize + 1);
-    });
+        error.response.statusText,
+        "Return code was not 'Bad Request'"
+      ).to.equal("Bad Request");
+    }
+  });
 
-    it("Verify that post errors if you provide non-json data", async function () {
-      let response;
-      try {
-        response = await axios.request(testData.post.negative);
-      } catch (error) {
-        expect(error.response.status, "Return code was not 400").to.equal(400);
-        expect(
-          error.response.statusText,
-          "Return code was not 'Bad Request'"
-        ).to.equal("Bad Request");
-      }
-    });
+  it("Verify that POST replaces existing resource", async function () {
+    let response = await sendRequest(testData.get.positive);
+    let getCurrentArraySize = await response.data.length;
+    await sendRequest(testData.post.replace);
+    response = await sendRequest(testData.get.positive);
+    let getNewArraySize = await response.data.length;
 
-    it("Verify that POST replaces existing resource", async function () {
-      let response = await sendRequest(testData.get.positive);
-      let getCurrentArraySize = await response.data.length;
-      //add a resource using PUT - total should be equal to getCurrentArraySize
-      // await sendRequest(testData.put.positive);
-      //replace a resource using PUT - total should be equal to getCurrentArraySize
-      await sendRequest(testData.post.replace);
-      response = await sendRequest(testData.get.positive);
-      let getNewArraySize = await response.data.length;
+    expect(
+      getNewArraySize,
+      "Your back-end did not accept new resource, the amount of resources stayed the same"
+    ).to.equal(getCurrentArraySize);
+    let result = response.data.find((item) => item.main_key === "w");
+    expect(result).to.deep.equal(testData.post.replace.data);
+  });
 
-      expect(
-        getNewArraySize,
-        "Your back-end did not accept new resource, the amount of resources stayed the same"
-      ).to.equal(getCurrentArraySize);
-      let result = response.data.find((item) => item.main_key === "w");
-      expect(result).to.deep.equal(testData.post.replace.data);
-    });
+  it("Verify that you cannot replace with POST without providing value", async function () {
+    try {
+      response = await axios.request(testData.post.invalidValue);
+    } catch (error) {
+      expect(error.response.status).to.equal(400);
+      expect(error.response.statusText).to.equal("Bad Request");
+      expect(error.response.data).to.equal("'value'");
+    }
+  });
 
-    it("Verify that you cannot replace with POST without providing value", async function () {
-      // await sendRequest(testData.put.positive);
-      try {
-        response = await axios.request(testData.post.invalid);
-      } catch (error) {
-        expect(error.response.status).to.equal(400);
-        expect(error.response.statusText).to.equal("Bad Request");
-        expect(error.response.data).to.equal("'value'");
-      }
-    });
+  it("Verify that you cannot replace with POST without providing main_key", async function () {
+    try {
+      response = await axios.request(testData.post.invalidMainkey);
+    } catch (error) {
+      expect(error.response.status).to.equal(400);
+      expect(error.response.statusText).to.equal("Bad Request");
+      expect(error.response.data).to.equal("'main_key'");
+    }
   });
 });
--- test/put.js
+++ test/put.js
@@ -3,97 +3,99 @@ import { expect } from "chai";
 import { sendRequest } from "./utility.js";
 import testData from "../request-data.json" assert { type: "json" };
 
-describe("Team Sava", function () {
-  describe("Verify PUT endpoint", function () {
-    afterEach(async function () {
-      await sendRequest(testData.put.delete);
-    });
+describe("Verify PUT endpoint", function () {
+  afterEach(async function () {
+    await sendRequest(testData.put.delete);
+  });
 
-    it("Verify that put returns 200 and OK", async function () {
-      let response = await sendRequest(testData.put.positive);
+  it("Verify that put returns 200 and OK", async function () {
+    let response = await sendRequest(testData.put.positive);
 
-      expect(
-        response.status,
-        "Return status was not 200, you have an error"
-      ).to.equal(200);
-      expect(response.statusText).to.equal("OK");
-    });
+    expect(
+      response.status,
+      "Return status was not 200, you have an error"
+    ).to.equal(200);
+    expect(response.statusText).to.equal("OK");
+  });
 
-    it("Verify that put returns right content-type", async function () {
-      let response = await sendRequest(testData.put.positive);
+  it("Verify that put returns right content-type", async function () {
+    let response = await sendRequest(testData.put.positive);
 
-      expect(
-        response.headers["content-type"],
-        "Content-type was not as expected"
-      ).to.equal("application/json");
-    });
+    expect(
+      response.headers["content-type"],
+      "Content-type was not as expected"
+    ).to.equal("application/json");
+  });
 
-    it("Verify that put returns added resource", async function () {
-      let response = await sendRequest(testData.put.positive);
+  it("Verify that put returns added resource", async function () {
+    let response = await sendRequest(testData.put.positive);
 
-      expect(response.data).to.deep.equal(testData.put.positive.data);
-    });
+    expect(response.data).to.deep.equal(testData.put.positive.data);
+  });
 
-    it("Verify that put request adds a resource", async function () {
-      let response = await sendRequest(testData.get.positive);
-      let getCurrentArraySize = await response.data.length;
-      // add a resource
-      await sendRequest(testData.put.positive);
-      // check new size
-      response = await axios.request(testData.get.positive);
+  it("Verify that put request adds a resource", async function () {
+    let response = await sendRequest(testData.get.positive);
+    let getCurrentArraySize = await response.data.length;
+    // add a resource
+    await sendRequest(testData.put.positive);
+    // check new size
+    response = await axios.request(testData.get.positive);
 
-      let getNewArraySize = await response.data.length;
-      expect(
-        getNewArraySize,
-        "Your back-end did not accept new resource, the amount of resources stayed the same"
-      ).to.equal(getCurrentArraySize + 1);
-    });
+    let getNewArraySize = await response.data.length;
+    expect(
+      getNewArraySize,
+      "Your back-end did not accept new resource, the amount of resources stayed the same"
+    ).to.equal(getCurrentArraySize + 1);
+  });
 
-    it("Verify that put replaces existing resource", async function () {
-      let response = await sendRequest(testData.get.positive);
-      let getCurrentArraySize = await response.data.length;
-      1;
-      //add a resource using PUT - total should be equal to getCurrentArraySize
-      await sendRequest(testData.put.positive);
-      //replace a resource using PUT - total should be equal to getCurrentArraySize
-      response = await sendRequest(testData.get.positive);
-      let getNewArraySize = await response.data.length;
+  it("Verify that put replaces existing resource", async function () {
+    let response = await sendRequest(testData.get.positive);
 
-      expect(
-        getNewArraySize,
-        "Your back-end did not accept new resource, the amount of resources stayed the same"
-      ).to.equal(getCurrentArraySize + 1);
-      let result = response.data.find((item) => item.main_key === "w");
-      expect(result).to.deep.equal(testData.put.replace.data);
-    });
+    await sendRequest(testData.put.positive);
+    //replace a resource using PUT
+    response = await sendRequest(testData.get.positive);
 
-    it("Verify error if quota is excedeed", async function () {
-      //TODO set up this
-      let response = await sendRequest(testData.get.positive);
-      let getCurrentArraySize = await response.data.length;
-      // add a resource, should fail
-      if (getCurrentArraySize >= 10) {
-        try {
-          await axios.request(testData.put.positive);
-          // check new size
-          response = await axios.request(testData.get.positive);
-        } catch (error) {
-          expect(error.response.status).to.equal(400);
-          expect(error.response.statusText).to.equal("Bad Request");
-          expect(error.response.data).to.equal("you reached your quta");
-        }
-      }
-    });
+    let result = response.data.find((item) => item.main_key === "w");
+    expect(result).to.deep.equal(testData.put.replace.data);
+  });
 
-    it("Verify error if you try to create a resource with PUT without value", async function () {
-      let response;
+  it("Verify error if quota is excedeed", async function () {
+    //TODO set up this
+    let response = await sendRequest(testData.get.positive);
+    let getCurrentArraySize = await response.data.length;
+    // add a resource, should fail
+    if (getCurrentArraySize >= 10) {
       try {
-        response = await axios.request(testData.put.invalid);
+        await axios.request(testData.put.positive);
+        // check new size
+        response = await axios.request(testData.get.positive);
       } catch (error) {
         expect(error.response.status).to.equal(400);
         expect(error.response.statusText).to.equal("Bad Request");
-        expect(error.response.data).to.equal("'value'");
+        expect(error.response.data).to.equal("you reached your quta");
       }
-    });
+    }
+  });
+
+  it("Verify error if you try to create a resource with PUT without value", async function () {
+    let response;
+    try {
+      response = await axios.request(testData.put.invalidValue);
+    } catch (error) {
+      expect(error.response.status).to.equal(400);
+      expect(error.response.statusText).to.equal("Bad Request");
+      expect(error.response.data).to.equal("'value'");
+    }
+  });
+
+  it("Verify error if you try to create a resource with PUT without main_key", async function () {
+    let response;
+    try {
+      response = await axios.request(testData.put.invalidMainkey);
+    } catch (error) {
+      expect(error.response.status).to.equal(400);
+      expect(error.response.statusText).to.equal("Bad Request");
+      expect(error.response.data).to.equal("'main_key'");
+    }
   });
 });
